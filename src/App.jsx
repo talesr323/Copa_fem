339c81334908b4831e87da237c42aeb3838f82f6
Tales <talesr323@gmail.com>
2024-09-19 20:34:22 -0300

    adicionando KnockoutStage

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Card from './components/Card'
 import Game_Table from './components/Game_Table'
 import GroupStandings from './components/GroupStandings'
+import KnockoutStage from './components/KnockoutStage'
 
 function App() {
 
@@ -21,10 +22,14 @@ function App() {
     <h2>Classificação por Grupo</h2>
 
     <section className='group_table'>
-      <h2>Grupo A</h2>
         <GroupStandings /> 
     </section>
 
+    <h2>Oitava de Final</h2>
+
+    <section className='knockout_table'>
+        <KnockoutStage fase="oitavas"/> 
+    </section>
     </>
   )
 }
--- src/components/GroupStandings/index.jsx
+++ src/components/GroupStandings/index.jsx
@@ -5,6 +5,7 @@ import styles from './GroupStandings.module.css'
 function GroupStandings() {
 
     const [grupos, setGrupos] = useState([])
+    const [ letraSelecionada, setLetraSelecionada] = useState('A')
 
     useEffect(() => {
         const buscarGrupos = async () => {
@@ -16,39 +17,55 @@ function GroupStandings() {
     }, [])
 
     return (
-        <table className={styles.table}>
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Seleção</th>
-                    <th>Pontos</th>
-                    <th>Vitorias</th>
-                    <th>Empates</th>
-                    <th>Derrotas</th>
-                    <th>Gols Pro</th>
-                    <th>Gols Contra</th>
-                    <th>Saldo Gols</th>
-                </tr>
-            </thead>
+        <>
+            <div className={styles.div_select}>
+                <select 
+                    value={letraSelecionada} 
+                    onChange={(e) => setLetraSelecionada(e.target.value)}>
+                    <option value="A"> Grupo A</option>
+                    <option value="B"> Grupo B</option>
+                    <option value="C"> Grupo C</option>
+                    <option value="D"> Grupo D</option>
+                    <option value="E"> Grupo E</option>
+                    <option value="F"> Grupo F</option>
+                    <option value="G"> Grupo G</option>
+                    <option value="H"> Grupo H</option>
+                </select>
+            </div>
+            <table className={styles.table}>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Seleção</th>
+                        <th>Pontos</th>
+                        <th>Vitorias</th>
+                        <th>Empates</th>
+                        <th>Derrotas</th>
+                        <th>Gols Pro</th>
+                        <th>Gols Contra</th>
+                        <th>Saldo Gols</th>
+                    </tr>
+                </thead>
 
-            <tbody>
-                {
-                    grupos.filter( grupo => grupo.grupo === "A").map(grupo => (
-                        <tr key={grupo.selecao}>
-                            <td>{grupo.posicao}</td>
-                            <td className={styles.esquerda}>{grupo.selecao}</td>
-                            <td>{grupo.pontos}</td>
-                            <td>{grupo.vitorias}</td>
-                            <td>{grupo.empates}</td>
-                            <td>{grupo.derrotas}</td>
-                            <td>{grupo.gols_pro}</td>
-                            <td>{grupo.gols_contra}</td>
-                            <td>{grupo.saldo_gols}</td>
-                        </tr>
-                    ))
-                }
-            </tbody>
-        </table>
+                <tbody>
+                    {
+                        grupos.filter( grupo => grupo.grupo === letraSelecionada).map(grupo => (
+                            <tr key={grupo.selecao}>
+                                <td>{grupo.posicao}</td>
+                                <td className={styles.esquerda}>{grupo.selecao}</td>
+                                <td>{grupo.pontos}</td>
+                                <td>{grupo.vitorias}</td>
+                                <td>{grupo.empates}</td>
+                                <td>{grupo.derrotas}</td>
+                                <td>{grupo.gols_pro}</td>
+                                <td>{grupo.gols_contra}</td>
+                                <td>{grupo.saldo_gols}</td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+        </>
     )
 }
 
